ab3cefdc5a24e8a12263ceef9f53499746c94121
ixrec <ericrhitchcock@gmail.com>
2023-12-24 18:18:58 +0000

    wip

---
+++ worlds/outer_wilds/Options.py
@@ -0,0 +1,29 @@
+from dataclasses import dataclass
+
+from Options import Choice, Toggle, PerGameCommonOptions
+
+
+class Goal(Choice):
+    """The victory condition for your Archipelago run.
+    Song of Five: Reach the Eye and gather the 5 Hearthian travelers
+    Song of Six: Reach the Eye and gather 6 travelers (5 Hearthians and Solanum)."""
+    display_name = "Goal"
+    option_song_of_five = 0
+    option_song_of_six = 1
+
+
+class DeathLink(Toggle):
+    """When you die, everyone dies. Of course the reverse is true too."""
+    display_name = "Death Link"
+
+
+class Textsanity(Toggle):
+    """Every line of Nomai text is a check. You've been warned."""
+    display_name = "Textsanity"
+
+
+@dataclass
+class OuterWildsGameOptions(PerGameCommonOptions):
+    goal: Goal
+    death_link: DeathLink
+    textsanity: Textsanity

2050adb936fd909545516441976e3213f71474ac
Ixrec <ericrhitchcock@gmail.com>
2024-01-06 07:28:38 +0000

    add more comments to RuleEval.py, to prepare for tracker contributions

--- worlds/outer_wilds/RuleEval.py
+++ worlds/outer_wilds/RuleEval.py
@@ -3,6 +3,14 @@ from typing import Any
 
 from BaseClasses import CollectionState
 
+# In the .jsonc files we use, a location or region connection's "access rule" is defined
+# by a "requires" key, whose value is an array of "criteria" strings or objects.
+# These rules are designed to be evaluated by both this Python code and
+# (in the future) the game mod's C# code for the in-game tracker.
+
+# In particular: this eval_rule() function is the main piece of code which will have to
+# be implemented in both languages, so it's important we keep the implementations in sync
+
 
 def eval_rule(state: CollectionState, p: int, rule: [Any]) -> bool:
     return all(eval_criterion(state, p, criterion) for criterion in rule)
@@ -18,10 +26,14 @@ def eval_criterion(state: CollectionState, p: int, criterion: Any) -> bool:
         if len(criterion.items()) != 1:
             return False
         key, value = next(iter(criterion.items()))
+
+        # { "anyOf": [ ... ] } and { "location": "foo" } and { "region": "bar" } mean exactly
+        # what they sound like, and those are the only kinds of non-string criteria.
         if key == "anyOf" and isinstance(value, list):
             return any(eval_criterion(state, p, sub_criterion) for sub_criterion in value)
         elif key == "location" and isinstance(value, str):
             return state.can_reach(value, "Location", p)
         elif key == "region" and isinstance(value, str):
             return state.can_reach(value, "Region", p)
+
     raise ValueError("Unable to evaluate rule criterion: " + json.dumps(criterion))

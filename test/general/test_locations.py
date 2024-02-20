664308e960b012727036cae955a11140f85773af
Ixrec <ericrhitchcock@gmail.com>
2024-02-20 05:44:40 +0000

    restrict some core tests (including core's invoking of world tests) to just my OW world

--- test/general/test_locations.py
+++ test/general/test_locations.py
@@ -8,6 +8,8 @@ class TestBase(unittest.TestCase):
     def test_create_duplicate_locations(self):
         """Tests that no two Locations share a name or ID."""
         for game_name, world_type in AutoWorldRegister.world_types.items():
+            if game_name != "Outer Wilds":
+                continue
             multiworld = setup_solo_multiworld(world_type)
             locations = Counter(location.name for location in multiworld.get_locations())
             if locations:
@@ -23,6 +25,8 @@ class TestBase(unittest.TestCase):
     def test_locations_in_datapackage(self):
         """Tests that created locations not filled before fill starts exist in the datapackage."""
         for game_name, world_type in AutoWorldRegister.world_types.items():
+            if game_name != "Outer Wilds":
+                continue
             with self.subTest("Game", game_name=game_name):
                 multiworld = setup_solo_multiworld(world_type)
                 locations = multiworld.get_unfilled_locations()  # do unfilled locations to avoid Events
@@ -34,6 +38,8 @@ class TestBase(unittest.TestCase):
         """Tests that Regions and Locations aren't created after `create_items`."""
         gen_steps = ("generate_early", "create_regions", "create_items")
         for game_name, world_type in AutoWorldRegister.world_types.items():
+            if game_name != "Outer Wilds":
+                continue
             with self.subTest("Game", game_name=game_name):
                 multiworld = setup_solo_multiworld(world_type, gen_steps)
                 region_count = len(multiworld.get_regions())
@@ -66,6 +72,8 @@ class TestBase(unittest.TestCase):
     def test_location_group(self):
         """Test that all location name groups contain valid locations and don't share names."""
         for game_name, world_type in AutoWorldRegister.world_types.items():
+            if game_name != "Outer Wilds":
+                continue
             with self.subTest(game_name, game_name=game_name):
                 for group_name, locations in world_type.location_name_groups.items():
                     with self.subTest(group_name, group_name=group_name):

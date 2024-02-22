696ef73b34d53f962c6fa758595761688bf5f9b2
Ixrec <ericrhitchcock@gmail.com>
2024-02-22 02:51:47 +0000

    slightly improve location rule eval times by giving 'Space' region only one entrance

--- test/benchmark/locations.py
+++ test/benchmark/locations.py
@@ -50,6 +50,8 @@ def run_locations_benchmark():
 
         def main(self):
             for game in sorted(AutoWorld.AutoWorldRegister.world_types):
+                if game != "Outer Wilds":
+                    continue
                 summary_data: typing.Dict[str, collections.Counter[str]] = {
                     "empty_state": collections.Counter(),
                     "all_state": collections.Counter(),

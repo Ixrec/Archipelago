461c424036f0779b212a683dba3eb8aaba50ea68
Ixrec <ericrhitchcock@gmail.com>
2024-03-16 13:28:33 +0000

    set version to 0.1.5

--- worlds/outer_wilds/__init__.py
+++ worlds/outer_wilds/__init__.py
@@ -84,7 +84,7 @@ class OuterWildsWorld(World):
         # slot_data["eotu_coordinates"] = self.eotu_coordinates
         # Archipelago does not yet have apworld versions (data_version is deprecated),
         # so we have to roll our own with slot_data for the time being
-        slot_data["apworld_version"] = "0.1.4"
+        slot_data["apworld_version"] = "0.1.5"
         return slot_data
 
     # def write_spoiler(self, spoiler_handle: TextIO) -> None:

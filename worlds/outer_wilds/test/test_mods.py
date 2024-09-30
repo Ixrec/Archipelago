d955f1972eee1c3a9e014998a11d2ab5a5d772e9
Ixrec <ericrhitchcock@gmail.com>
2024-09-30 18:58:01 +0100

    options, items, locations and count tests for Astral Codec and Outsider

--- worlds/outer_wilds/test/test_mods.py
+++ worlds/outer_wilds/test/test_mods.py
@@ -22,29 +22,77 @@ class TestHN1Logsanity(OuterWildsTestBase):
         self.assertEqual(self.getLocationCount(), 326)
 
 
+class TestOutsider(OuterWildsTestBase):
+    options = {
+        "enable_outsider_mod": 1
+    }
+
+    def test_outsider(self):
+        self.assertEqual(self.getLocationCount(), 110)  # 87(+2V) base game + 21 TO locations
+
+
+class TestOutsiderLogsanity(OuterWildsTestBase):
+    options = {
+        "enable_outsider_mod": 1,
+        "logsanity": 1
+    }
+
+    def test_outsider_logsanity(self):
+        # 87(+2V) base game default locations + 176 base game logsanity locations +
+        # 21 TO default locations + 44 TO logsanity locations
+        self.assertEqual(self.getLocationCount(), 330)
+
+
+class TestAC(OuterWildsTestBase):
+    options = {
+        "enable_ac_mod": 1
+    }
+
+    def test_ac(self):
+        self.assertEqual(self.getLocationCount(), 110)  # 87(+2V) base game + 21 AC locations
+
+
+class TestACLogsanity(OuterWildsTestBase):
+    options = {
+        "enable_ac_mod": 1,
+        "logsanity": 1
+    }
+
+    def test_ac_logsanity(self):
+        # 87(+2V) base game default locations + 176 base game logsanity locations +
+        # 21 AC default locations + 38 AC logsanity locations
+        self.assertEqual(self.getLocationCount(), 324)
+
+
 class TestAllMods(OuterWildsTestBase):
     options = {
         "enable_hn1_mod": 1,
-        # todo
+        "enable_outsider_mod": 1,
+        "enable_ac_mod": 1
     }
 
-    def test_all_mods_logsanity(self):
+    def test_all_mods(self):
         # 87(+2V) base game default locations +
         # 20 HN1 default locations +
-        self.assertEqual(self.getLocationCount(), 109)
+        # 21 TO default locations +
+        # 21 AC locations
+        self.assertEqual(self.getLocationCount(), 151)
 
 
 class TestAllModsLogsanity(OuterWildsTestBase):
     options = {
         "enable_hn1_mod": 1,
-        # todo
+        "enable_outsider_mod": 1,
+        "enable_ac_mod": 1,
         "logsanity": 1
     }
 
     def test_all_mods_logsanity(self):
         # 87(+2V) base game default locations + 176 base game logsanity locations +
-        # 20 HN1 default locations + 41 HN1 logsanity locations
-        self.assertEqual(self.getLocationCount(), 326)
+        # 20 HN1 default locations + 41 HN1 logsanity locations +
+        # 21 TO default locations + 44 TO logsanity locations +
+        # 21 AC default locations + 38 AC logsanity locations
+        self.assertEqual(self.getLocationCount(), 450)
 
 
 # this is just to get an assertion on the maximum possible location count
@@ -52,12 +100,15 @@ class TestAllModsAndDLCLogsanity(OuterWildsTestBase):
     options = {
         "enable_eote_dlc": 1,
         "enable_hn1_mod": 1,
-        # todo
+        "enable_outsider_mod": 1,
+        "enable_ac_mod": 1,
         "logsanity": 1
     }
 
     def test_all_mods_and_dlc_logsanity(self):
         # 87(+2V) base game default locations + 176 base game logsanity locations +
         # 34(+4V) DLC default locations + 72 DLC logsanity locations +
-        # 20 HN1 default locations + 41 HN1 logsanity locations
-        self.assertEqual(self.getLocationCount(), 436)
+        # 20 HN1 default locations + 41 HN1 logsanity locations +
+        # 21 TO default locations + 44 TO logsanity locations +
+        # 21 AC default locations + 38 AC logsanity locations
+        self.assertEqual(self.getLocationCount(), 560)

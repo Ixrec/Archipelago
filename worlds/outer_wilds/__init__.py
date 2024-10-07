e56e14e99dfa7ff8fe588ca1c519e9c7869d9534
Ixrec <ericrhitchcock@gmail.com>
2024-10-07 03:05:45 +0100

    forgot to add new mod options to slot_data

--- worlds/outer_wilds/__init__.py
+++ worlds/outer_wilds/__init__.py
@@ -146,7 +146,7 @@ class OuterWildsWorld(World):
             "death_link",                   # a client/mod feature
             "goal", "spawn",                             # affects tons of stuff, but also a client/mod faeture
             "logsanity", "enable_eote_dlc", "dlc_only",  # changes AP locations, needed by in-game tracker
-            "enable_hn1_mod",
+            "enable_hn1_mod", "enable_outsider_mod", "enable_ac_mod",
             "split_translator"                           # changes AP items, and how client/mod implements Translator
         )
         # more client/mod features, these are only in the apworld because we want them fixed per-slot/at gen time

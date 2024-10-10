a19c4853c89cf88d4cd2d95ae2babee3ed72df30
Ixrec <ericrhitchcock@gmail.com>
2024-10-10 22:56:19 +0100

    bump version to 0.3.3

--- worlds/outer_wilds/__init__.py
+++ worlds/outer_wilds/__init__.py
@@ -158,7 +158,7 @@ class OuterWildsWorld(World):
         slot_data["warps"] = self.warps
         # Archipelago does not yet have apworld versions (data_version is deprecated),
         # so we have to roll our own with slot_data for the time being
-        slot_data["apworld_version"] = "0.3.2"
+        slot_data["apworld_version"] = "0.3.3"
         return slot_data
 
     def write_spoiler(self, spoiler_handle: TextIO) -> None:

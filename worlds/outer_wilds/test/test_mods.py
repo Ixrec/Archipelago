7d029ea81f482a2ccc2341639eb68f5e04a7fe9c
Ixrec <ericrhitchcock@gmail.com>
2024-10-15 21:51:22 +0100

    update enable_fq_mod description with location counts, and add/update mod tests to assert those location counts are correct

--- worlds/outer_wilds/test/test_mods.py
+++ worlds/outer_wilds/test/test_mods.py
@@ -85,12 +85,34 @@ class TestHN2Logsanity(OuterWildsTestBase):
         self.assertEqual(self.getLocationCount(), 313)
 
 
+class TestFQ(OuterWildsTestBase):
+    options = {
+        "enable_fq_mod": 1
+    }
+
+    def test_fq(self):
+        self.assertEqual(self.getLocationCount(), 107)  # 87(+2V) base game + 18 FQ locations
+
+
+class TestFQLogsanity(OuterWildsTestBase):
+    options = {
+        "enable_fq_mod": 1,
+        "logsanity": 1
+    }
+
+    def test_fq_logsanity(self):
+        # 87(+2V) base game default locations + 176 base game logsanity locations +
+        # 18 FQ default locations + 38 FQ logsanity locations
+        self.assertEqual(self.getLocationCount(), 321)
+
+
 class TestAllMods(OuterWildsTestBase):
     options = {
         "enable_hn1_mod": 1,
         "enable_outsider_mod": 1,
         "enable_ac_mod": 1,
-        "enable_hn2_mod": 1
+        "enable_hn2_mod": 1,
+        "enable_fq_mod": 1,
     }
 
     def test_all_mods(self):
@@ -98,8 +120,9 @@ class TestAllMods(OuterWildsTestBase):
         # 20 HN1 default locations +
         # 21 TO default locations +
         # 21 AC locations +
-        # 18 HN2 locations
-        self.assertEqual(self.getLocationCount(), 169)
+        # 18 HN2 locations +
+        # 18 FQ default locations
+        self.assertEqual(self.getLocationCount(), 187)
 
 
 class TestAllModsLogsanity(OuterWildsTestBase):
@@ -108,6 +131,7 @@ class TestAllModsLogsanity(OuterWildsTestBase):
         "enable_outsider_mod": 1,
         "enable_ac_mod": 1,
         "enable_hn2_mod": 1,
+        "enable_fq_mod": 1,
         "logsanity": 1
     }
 
@@ -116,8 +140,9 @@ class TestAllModsLogsanity(OuterWildsTestBase):
         # 20 HN1 default locations + 41 HN1 logsanity locations +
         # 21 TO default locations + 44 TO logsanity locations +
         # 21 AC default locations + 39 AC logsanity locations +
-        # 18 HN2 default locations + 30 HN2 logsanity locations
-        self.assertEqual(self.getLocationCount(), 499)
+        # 18 HN2 default locations + 30 HN2 logsanity locations +
+        # 18 FQ default locations + 38 FQ logsanity locations
+        self.assertEqual(self.getLocationCount(), 555)
 
 
 # this is just to get an assertion on the maximum possible location count
@@ -128,6 +153,7 @@ class TestAllModsAndDLCLogsanity(OuterWildsTestBase):
         "enable_outsider_mod": 1,
         "enable_ac_mod": 1,
         "enable_hn2_mod": 1,
+        "enable_fq_mod": 1,
         "logsanity": 1
     }
 
@@ -137,5 +163,6 @@ class TestAllModsAndDLCLogsanity(OuterWildsTestBase):
         # 20 HN1 default locations + 41 HN1 logsanity locations +
         # 21 TO default locations + 44 TO logsanity locations +
         # 21 AC default locations + 39 AC logsanity locations +
-        # 18 HN2 default locations + 30 HN2 logsanity locations
-        self.assertEqual(self.getLocationCount(), 609)
+        # 18 HN2 default locations + 30 HN2 logsanity locations +
+        # 18 FQ default locations + 38 FQ logsanity locations
+        self.assertEqual(self.getLocationCount(), 665)

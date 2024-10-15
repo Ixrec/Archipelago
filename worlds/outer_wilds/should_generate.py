d9302416c44429447bbd871e5f0ae4135efcafe4
Ixrec <ericrhitchcock@gmail.com>
2024-10-15 17:37:53 +0100

    teach the should_generate() function that category: 'fq' content should be generated if and only if the player set 'enable_fq_mod' to true

--- worlds/outer_wilds/should_generate.py
+++ worlds/outer_wilds/should_generate.py
@@ -29,4 +29,6 @@ def should_generate(category: Optional[str], options: OuterWildsGameOptions) -> 
         return options.enable_ac_mod.value == 1
     elif category == 'hn2':
         return options.enable_hn2_mod.value == 1
+    elif category == 'fq':
+        return options.enable_fq_mod.value == 1
     raise ValueError(f'Invalid category: {category}')

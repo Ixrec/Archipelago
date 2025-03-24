d8cfb43278f58bbf36a1eacffb684fe8b5323537
Ixrec <ericrhitchcock@gmail.com>
2025-03-24 00:17:18 +0000

    first note-taking session

--- worlds/nine_sols/options.py
+++ worlds/nine_sols/options.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 
-from Options import DefaultOnToggle, PerGameCommonOptions, StartInventoryPool, Toggle
+from Options import Choice, DefaultOnToggle, PerGameCommonOptions, StartInventoryPool, Toggle
 
 
 class ShuffleSolSeals(DefaultOnToggle):
@@ -14,9 +14,38 @@ class SkipSoulscapePlatforming(Toggle):
     display_name = "Skip Soulscape Platforming"
 
 
+class LogicDifficulty(Choice):
+    """
+    `vanilla` is exactly what it sounds like: You will only be expected to do what the vanilla game required.
+
+    `easy` adds tricks that are no harder to execute than what the vanilla game requires,
+    once you've been told these tricks exist. Specifically:
+    - Using a Cloud Piercer S (or X) arrow to break Charged Strike barriers without Charged Strike
+    - Using a Thunder Buster arrow (any level) to break one-way barriers from the "wrong" side
+    - "Bow hover": Press and hold jump, shoot the bow immediately (during the first half of Yi's upward movement) with
+    any arrow equipped, and then simply never let go of the jump button until you're done hovering.
+    - "Pseudo Air Dashes" using either talismans or Charged Strike
+
+    Although these are as "easy" as vanilla, they may be more frustrating than vanilla, since missing a talisman dash
+    or bow hover may involve significant backtracking to make another attempt.
+
+    Bow and talisman logic will assume you only have the initial 2 arrows and 1 qi to spend in between root nodes and
+    parryable enemies. Any route that requires more arrows or more qi (e.g. breaking one-way barriers with 3 Cloud
+    Piercer arrows, traversing the bottom of AF (Depths) with only talisman dashes) will simply be out of logic.
+
+    `oops_all_ledge_storage`: TODO
+    """
+    display_name = "Logic Difficulty"
+    option_vanilla = 0
+    option_easy = 1
+    option_oops_all_ledge_storage = 2
+    default = 0
+
+
 @dataclass
 class NineSolsGameOptions(PerGameCommonOptions):
     start_inventory_from_pool: StartInventoryPool
     shuffle_sol_seals: ShuffleSolSeals
     skip_soulscape_platforming: SkipSoulscapePlatforming
+    logic_difficulty: LogicDifficulty
 

30199b7f5f2b50ff94115b6d577bc6dbac0fafd9
Ixrec <ericrhitchcock@gmail.com>
2025-12-13 15:01:48 +0000

    .

--- worlds/AutoWorld.py
+++ worlds/AutoWorld.py
@@ -92,12 +92,7 @@ class AutoWorldRegister(type):
                     # all the "magic" game names that core AP tests assume exist and fail without
                     or dct["game"] == "APQuest"
                     or dct["game"] == "Archipelago"
-                    or dct["game"] == "Test Game"
-                    # this is only required by the CI-only hosting/__main__.py test
-                    or dct["game"] == "Temp World"
-                    # and finally, this one is only required by the CI-only "Build" jobs because build.yml
-                    # assumes that Generate Template Options will produce a VVVVVV.yaml file
-                    or dct["game"] == "VVVVVV"):
+                    or dct["game"] == "Test Game"):
                 AutoWorldRegister.world_types[dct["game"]] = new_class
         if ".apworld" in new_class.__file__:
             new_class.zip_path = pathlib.Path(new_class.__file__).parents[1]
